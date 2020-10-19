84edc2e283b879502b3478a0d994533956476ab9
Gabriel Nistor <gabriel.nistor1991@gmail.com>
2020-10-19 16:55:32 +0100

    first commit

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+function showHide() {
+    var keyboard = document.getElementById("big-keyboard");
+
+    if (keyboard.className === "show") {
+        keyboard.className = "hide";
+    } else {
+        keyboard.className = "show";
+    }
+}
+
